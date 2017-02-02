70f222f96689e88f3aa15f71312d2d13913512f3
SPEC1AL1ST <spec.ua@gmail.com>
2017-02-02 20:45:00 +0200

    W3C Validator 2

--- auto/success.php
+++ auto/success.php
@@ -354,7 +354,7 @@
 <!--Slider-->
 <script>
     function openNav() {
-        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("mySidenav").style.width = "210px";
     }
 
     function closeNav() {
--- faq.php
+++ faq.php
@@ -322,7 +322,7 @@
 <!--Slider-->
 <script>
     function openNav() {
-        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("mySidenav").style.width = "210px";
     }
 
     function closeNav() {
